80b982ebb4fd28912fb25774dcfd81a0e25633a6
Jason Turner <jasonalmaturner@gmail.com>
2015-01-11 21:21:31 -0700

    adding alternative solutions

--- arraysPractice.js
+++ arraysPractice.js
@@ -47,6 +47,12 @@ function reversedLooper(arr) {
   }
 }
 
+var reversedLooper2 = function(arr) {
+  for(var i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+  }
+}
+
 
 //Next Problem
 
